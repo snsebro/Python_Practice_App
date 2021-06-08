02d2c838726e8d40d3d9621b9cdb9802936ec5e9
Shayne Sebro <shaynesebro@gmail.com>
2021-06-07 21:54:06 -0400

    importing sys module to determine if user is using mac os, if they are do not include filetypes argument in askopenfilename()

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import tkinter as tk
 from tkinter import filedialog, Text
+import sys
 import os
 
 root = tk.Tk()
+apps = []
+filename = ''
+
+os = sys.platform
+print(os)
 
 
 def addApp():
-    filename = filedialog.askopenfilename(
-        initialdir="/", title="Select File", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    if os == 'darwin':
+        filename = filedialog.askopenfilename(
+            initialdir="/applications", title="Select File")
+        apps.append(filename)
+    else:
+        filename = filedialog.askopenfilename(
+            initialdir="/", title="Select File", filetypes=(("executables", "*.exe", ("all files", "*.*"))))
+        apps.append(filename)
+
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="white")
+        label.pack()
 
 
 canvas = tk.Canvas(root, height=700, width=700)
